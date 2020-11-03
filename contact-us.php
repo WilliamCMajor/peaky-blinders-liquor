032d6d793b4d537f40e74e09d4b8e7eaf2feb8d8
WilliamCMajor <she8@nait.ca>
2020-11-03 02:03:58 -0700

    rewrite page, fixed bug

--- contact-us.php
+++ contact-us.php
@@ -1,49 +1,11 @@
-<?php
-include("includes/header.php");
-?>
-
-<?php
-   //session_start();
-    $title = $_POST['title'];
-    $message = $_POST['message'];
-    $timedate = date('l, F d, Y @ g:i a');
-    
-
-    if(($title !="") && ($message != "")){
-        writeToBlog($title, $message, $timedate);
-    }
-    
-
-    function writeToBlog($thisTitle, $thisMessage, $thisTimedate){
-        $handle = fopen("blogfile.txt", "r");
-        if($handle){
-            while(!feof($handle)){
-                $buffer = fgets($handle, 4096);
-                //$existingText .= $buffer;
-            }// end while
-            //echo $existingText;
-            fclose($handle);
-        }// end if
-        
-        
-        if(isset($_POST['submit'])) {      
-            $handle = fopen("blogfile.txt", "w");
-            $newStuff = "\n<div class=\"newEntry\">";
-            $newStuff .= "\n\t<div class=\"title\">" . $thisTitle . "</div>";
-            $newStuff .= "\n\t<div class=\"timedate\">" . $thisTimedate . "</div>";
-            $newStuff .= "\n\t<div class=\"message\">" . $thisMessage . "</div>";
-            $newStuff .= "\n</div>";
-            $allStuff = $newStuff . $buffer;
-            fwrite($handle, $allStuff);
-            fclose($handle);
-        }
-    }// end writeToBlog   
-?>
+<?php include("includes/header.php"); ?>
 
     <main>
         <div class="comment-board">
-            <h2>Comment Board</h2>
-            <?php include("blogfile.txt"); ?>
+            <h2>Comments Board</h2>
+            <p>Please leave any comments or feedback</p>
+            <hr>
+            <?php //include("blogfile.txt"); ?>
         </div>
         <form id="comment" method="post" action="#">
             <div class="form-container">
